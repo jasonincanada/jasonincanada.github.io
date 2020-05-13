0900c8d8591ce640716a6a26601be20868f033b7
jasonincanada <nirgle@gmail.com>
2020-05-13 18:35:54 -0400

    add react-in-space

---
+++ react-in-space/precache-manifest.47bef69ffd110d6674f17faf855b37d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179b45d41db779afd38a147f8c14fa7b",
+    "url": "/react-in-space/index.html"
+  },
+  {
+    "revision": "495411ec5691fd269416",
+    "url": "/react-in-space/static/css/main.50df4f22.chunk.css"
+  },
+  {
+    "revision": "4f8f850021ec85698a4f",
+    "url": "/react-in-space/static/js/2.080373c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-in-space/static/js/2.080373c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495411ec5691fd269416",
+    "url": "/react-in-space/static/js/main.9a32005a.chunk.js"
+  },
+  {
+    "revision": "3b631caeb3caa47dad9d",
+    "url": "/react-in-space/static/js/runtime-main.fcc8448f.js"
+  },
+  {
+    "revision": "f4d266f89bd834f47f095d9985f274b2",
+    "url": "/react-in-space/static/media/gradient.f4d266f8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-in-space/static/media/logo.5d5d9eef.svg"
+  }
+]);
